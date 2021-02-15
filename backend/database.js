85bd0acc8865a24a68ec65e97fd3d2b7a85921d3
abelandres30 <abel_andres30@hotmail.com>
2021-02-14 21:45:35 -0600

    Revert "Initial commit"
    This reverts commit a4528bb45ed8d0ef1992574c9766bde900724c37.
    git log --oneline

--- backend/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/proyecto-crud';
-mongoose.connect(URI)
-.then(db => console.log('db esta conectado'))
-.catch(err => console.log(err));
-module.exports = mongoose;
--- backend/routes/imagenes.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const upload = require('../libs/storage');
-const Imagenesctrl = require('../controllers/imagenes.controller');
-const router = express.Router();
-
-router.get('/', Imagenesctrl.getImagenes);
-router.post('/', Imagenesctrl.createImagenes);
-router.delete('/:id', Imagenesctrl.deleteImagenes);
-module.exports = router;
